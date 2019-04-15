64c0d0b9c37d59b854a2da5f225359cdec8fdd40
shaliniqa <40268047+shaliniqa@users.noreply.github.com>
2019-04-15 18:10:25 +0530

    Update FindKthSmallestElement.java

--- HeapBasics/FindKthSmallestElement.java
+++ HeapBasics/FindKthSmallestElement.java
@@ -3,9 +3,9 @@ package HeapBasics;
 public class FindKthSmallestElement {
 	public static void main(String args[])
 	{
-		int arr[]= {2,6,7,9,3};
+		int arr[]= {2,6,7,9,1,4};
 		FindKthSmallestElement obj=new FindKthSmallestElement();
-		obj.kth_smallest(arr,5,1);
+		obj.kth_smallest(arr,6,3);
 		
 	}
 	 public void  kth_smallest(int arr[],int n, int k)
@@ -16,10 +16,16 @@ public class FindKthSmallestElement {
 		 {
 			 heap.add(arr[i]);
 		 }
+//		 heap.display();
+//		 heap.remove();
+//		 heap.display();
+//		 heap.remove();
+//		 heap.display();
+//		 heap.remove();
+//		 heap.display();
 		 for(int i=0;i<k;i++)
 		 {
-			 System.out.println(heap.extractMin());
-			 heap.remove();
+			 System.out.println(heap.remove());
 		 }
 	 }
 	
